9f33f6a6d92943e1bba454962895378ae892fabb
Samuel Harvey <samuelharvey06@gmail.com>
2026-02-06 13:53:17 -0500

    Removed unused "Enregistrer" button from account page for cleaner UI.

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -51,9 +51,6 @@ export default function AccountPage() {
                                     <p className="text-lg mt-1">{user.user_metadata.last_name}</p>
                                 </div>
                             </div>
-
-                            <button className="cursor-pointer px-5 h-full py-2 rounded-lg bg-white/5 border border-white/10 hover:bg-white/10 transition-all group">Enregistrer</button>
-
                             <p className="text-neutral-600 text-xs">Rejoint le {new Date(user.created_at).toLocaleDateString()}</p>
 
                             <div className="space-y-3">
